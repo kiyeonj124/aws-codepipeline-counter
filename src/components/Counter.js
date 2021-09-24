8a33f5c941df2a08af6b399a3b320abe47e6b31e
KiyeonJeon <kiyeonj124@gmail.com>
2021-09-24 13:11:52 +0900

    updated counter component to include -2 and +2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,14 +4,22 @@ const Counter = () => {
     const addOneToCounter=()=>{
         setCountValue(countValue+1)
     }
+    const addTwoToCounter=()=>{
+        setCountValue(countValue+2)
+    }
     const subtractOneToCounter=()=>{
         setCountValue(countValue-1)
+    }
+    const subtractTwoToCounter=()=>{
+        setCountValue(countValue-2)
     }
     return (
         <div className='container'>
             <h2 data-testid="header">Count: {countValue}</h2>
+            <button data-testid="subtract-two-btn" className='btn' onClick={subtractTwoToCounter}>-2</button>
             <button data-testid="subtract-one-btn" className='btn' onClick={subtractOneToCounter}>-1</button>
             <button data-testid="add-one-btn" className='btn' onClick={addOneToCounter} >+1</button>
+            <button data-testid="add-two-btn" className='btn' onClick={addTwoToCounter} >+2</button>
         </div>
     )
 }
--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -7,7 +7,9 @@ test("couter component works properly", ()=>{
     const { getByTestId} = render(<Counter />)
     const headerEl = getByTestId("header")
     const addOneBtnEl = getByTestId("add-one-btn")
+    const addTwoBtnEl = getByTestId("add-two-btn")
     const subtractOneBtnEl = getByTestId("subtract-one-btn")
+    const subtractTwoBtnEl = getByTestId("subtract-two-btn")
 
     expect(headerEl).toHaveTextContent("0")
 
@@ -23,17 +25,37 @@ test("couter component works properly", ()=>{
 
     expect(headerEl).toHaveTextContent("3")
 
-    fireEvent.click(subtractOneBtnEl);
-
-    expect(headerEl).toHaveTextContent("2")
-
-    fireEvent.click(subtractOneBtnEl);
+    fireEvent.click(subtractTwoBtnEl);
 
     expect(headerEl).toHaveTextContent("1")
 
+    fireEvent.click(subtractTwoBtnEl);
+
+    expect(headerEl).toHaveTextContent("-1")
+
     fireEvent.click(subtractOneBtnEl);
 
+    expect(headerEl).toHaveTextContent("-2")
+
+    fireEvent.click(subtractOneBtnEl);
+
+    expect(headerEl).toHaveTextContent("-3")
+
+    fireEvent.click(subtractOneBtnEl);
+
+    expect(headerEl).toHaveTextContent("-4")
+
+    fireEvent.click(addTwoBtnEl);
+
+    expect(headerEl).toHaveTextContent("-2")
+
+    fireEvent.click(addTwoBtnEl);
+
     expect(headerEl).toHaveTextContent("0")
+
+    fireEvent.click(addTwoBtnEl);
+
+    expect(headerEl).toHaveTextContent("2")
 
 })
 
